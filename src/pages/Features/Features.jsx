ff715364af6f5e5a11cccf1339515743a7943dc7
ey2ron <eyrownklayd24@gmail.com>
2025-11-04 11:02:02 +0800

    fixed typo errors in components

--- src/pages/Features/Features.jsx
+++ src/pages/Features/Features.jsx
@@ -1,4 +1,4 @@
-import { Navigation } from "../../components/navigation/Navigation";
+import { Navigation } from "../../components/Navigation/Navigation";
 
 export function Features() {
   return (
--- src/pages/Landing/LandingPage.jsx
+++ src/pages/Landing/LandingPage.jsx
@@ -1,5 +1,5 @@
 import "./LandingPage.css";
-import { Navigation } from "../../components/navigation/Navigation";
+import { Navigation } from "../../components/Navigation/Navigation";
 import { useNavigate } from "react-router-dom"; 
 
 export function LandingPage() {
